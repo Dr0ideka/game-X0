114dba9aafd4f93d8f1476d4662a353252f843c9
Dr0ideka <129878817+Dr0ideka@users.noreply.github.com>
2023-06-01 16:50:58 +0500

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -27,7 +27,7 @@ class Convertor:
             amount = float(amount)
         except ValueError:
             raise APIException(f'Не удалось обработать количество {amount}!')
-        r = requests.get(f"https://api.exchangeratesapi.io/latest?base={base_key}&symbols={sym_key}")
+        r = requests.get(f"http://api.exchangeratesapi.io/latest?base={base_key}&symbols={sym_key}")
         resp = json.loads(r.content)
         new_price = resp['rates'][sym_key] * amount
         new_price = round(new_price, 3)
